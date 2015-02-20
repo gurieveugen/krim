9580b57f9e7ad9faab48156a88d30d3d7f7b80a6
SeeJay <admin@gurievcreative>
2015-02-20 21:34:48 +0200

    1

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Krim</title>
+	<link rel="stylesheet" href="bootstrap/less/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="fonts/Circe-Bold.css">
+	<link rel="stylesheet" href="fonts/Circe-ExtraBold.css">
+	<link rel="stylesheet" href="fonts/Circe-ExtraLight.css">
+	<link rel="stylesheet" href="fonts/Circe-Light.css">
+	<link rel="stylesheet" href="fonts/Circe-Regular.css">
+	<link rel="stylesheet" href="fonts/Circe-Thin.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/fontsmoothie.min.js" async></script>
+</head>
+<body>
+	<header class="main">
+		<nav class="navbar navbar-yellow">
+		    <div class="container">
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		            <ul class="nav navbar-nav">
+		                <li class="active"><a href="#">Главная</a></li>
+		                <li><a href="#about">Контакты</a></li>
+		                <li class="dropdown">
+		                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+		                        Пример выпадающего списка
+		                        <span class="caret"></span>
+		                    </a>
+		                    <ul class="dropdown-menu" role="menu">
+		                        <li>
+		                            <a href="#">Пример 1</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 2</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 3</a>
+		                        </li>
+		                        <li class="divider"></li>
+		                        <li class="dropdown-header">Пример 5</li>
+		                        <li>
+		                            <a href="#">Пример 6</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 7</a>
+		                        </li>
+		                    </ul>
+		                </li>
+		                <li class="pull-right">
+		                	<a href="#"><i class="sprite sprite-icon_search"></i></a>
+		                </li>
+		            </ul>
+		        </div>
+		        <!--/.nav-collapse --> 
+		    </div>
+		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
+					<a href="/" class="logo"><img src="images/logo.png" alt="Logo"></a>
+				</div>
+				<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
+					<div class="switchers pull-right">
+						<ul class="socials">
+							<li><a href="#"><i class="sprite sprite-icon_facebook"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_twitter"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_vkontakte"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_odnoklassniki"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_youtube"></i></a></li>
+						</ul>
+						<ul class="languages">
+							<li><a href="#">Укр</a></li>
+							<li><a href="#" class="active">Рус</a></li>
+						</ul>	
+					</div>
+				</div>
+			</div>	
+			<ul class="categories">
+				<li class="active"><a href="">События</a></li>
+				<li><a href="">Нарушение прав человека</a></li>
+				<li><a href="">Бизнес-конфликты</a></li>
+				<li><a href="">Антифейк</a></li>
+				<li><a href="">История</a></li>
+				<li><a href="">Инфографика</a></li>
+				<li><a href="">Галерея</a></li>
+				<li><a href="">Мнение</a></li>
+			</ul>
+		</div>
+	</header>
+	<section class="main">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 gutter-right-half">
+					<div class="row">
+						<article class="featured">
+							<img src="images/placeholders/featured.jpg" alt="Thumbnail featured">
+							<header>
+								<small>5 февраля 2015</small>
+								<h1>Меджлис в изгнании</h1>
+								<h2>Кто стоит за рейдерским захватом Меджлиса. Хроника событий</h2>
+							</header>
+						</article>	
+					</div>
+					<div class="row gutter-top">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-right-half">
+							<article class="news">
+								<img src="images/placeholders/news_1.jpg" alt="News 1">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>В Ялте закрыли один из пляжей</h4>
+								</header>
+							</article>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-left-half">
+							<article class="news">
+								<img src="images/placeholders/news_2.jpg" alt="News 2">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>История крымской еврейской общины</h4>
+								</header>
+							</article>
+						</div>
+					</div>
+					<div class="row gutter-top">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-right-half">
+							<article class="news">
+								<img src="images/placeholders/news_3.jpg" alt="News 3">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Зависимость Крыма от Украины [инфографика]</h4>
+								</header>
+							</article>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-left-half">
+							<article class="news">
+								<img src="images/placeholders/news_4.jpg" alt="News 4">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Интервью Рефата Чубарова</h4>
+								</header>
+							</article>
+						</div>
+					</div>
+					<div class="row gutter-top">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-right-half">
+							<article class="news">
+								<img src="images/placeholders/news_2.jpg" alt="News 2">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Хасиды Алушты</h4>
+								</header>
+							</article>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-left-half">
+							<article class="news">
+								<img src="images/placeholders/news_1.jpg" alt="News 1">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Солнечная Ялта</h4>
+								</header>
+							</article>
+						</div>
+					</div>
+					<div class="row gutter-top">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-right-half">
+							<article class="news">
+								<img src="images/placeholders/news_1.jpg" alt="News 1">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Туризм в 205 году</h4>
+								</header>
+							</article>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 gutter-left-half">
+							<article class="news">
+								<img src="images/placeholders/news_2.jpg" alt="News 2">
+								<header>
+									<small>5 февраля 2015</small>
+									<h4>Еврейская община Гурзуфа</h4>
+								</header>
+							</article>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 gutter-left-half sidebar">
+					<ul class="small-news">
+						<li>
+							<figure>
+								<figcaption>
+									<small>5 февраля 2015</small>
+									<h1>Мост через Керченский пролив построят через 5 лет</h1>
+								</figcaption>
+								<img src="images/placeholders/sidebar_1.jpg" alt="small news 1">
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<figcaption>
+									<small>5 февраля 2015</small>
+									<h1>Мустафа Джемилев. Жизненная история крымскотатарского лидера</h1>
+								</figcaption>
+								<img src="images/placeholders/sidebar_2.jpg" alt="small news 2">
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<figcaption>
+									<small>5 февраля 2015</small>
+									<h1>Россия национализировала предприятия на полуострове</h1>
+								</figcaption>
+								<img src="images/placeholders/sidebar_3.jpg" alt="small news 3">
+							</figure>
+						</li>
+					</ul>
+					<div class="widget-tweets">
+						<span class="title">Tweets</span>
+						<img src="images/placeholders/tweets.jpg" alt="Tweets">
+					</div>
+					<div class="row gutter-top">
+						<img src="images/placeholders/facebook.jpg" alt="Facebook">
+					</div>
+					<div class="row gutter-top">
+						<article class="news">
+							<img src="images/placeholders/news_3.jpg" alt="News 3">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Зависимость Крыма от Украины [инфографика]</h4>
+							</header>
+						</article>
+					</div>
+					<div class="row gutter-top">
+						<article class="news">
+							<img src="images/placeholders/news_1.jpg" alt="News 1">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Восхождение на Чатыр-Даг</h4>
+							</header>
+						</article>
+					</div>
+				</div>
+			</div>
+			<div class="row featured-post-bottom gutter-top">
+				<div class="col-lg-8 col-md-8 col-xs-8 col-sm-8">
+					<img src="images/placeholders/history_1.jpg" alt="Featured post bottom">
+				</div>
+				<div class="col-lg-4 col-md-4 col-xs-4 col-sm-4">
+					<div class="text">
+						<small>5 февраля 2015</small>
+						<h3>Депортация крымских татар</h3>
+						Кто стоит за рейдерским захватом Меджлиса. Хроника событий
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="main">
+		<div class="container">
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<span class="slogan">Free crimea</span>
+				<span class="copyright">© 2015 Свободный Крым. Все права защищены</span>
+			</div>
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+				<ul class="footer-menu">
+					<li><a href="#">О проекте</a></li>
+					<li><a href="#">Контакты</a></li>
+					<li><a href="#">Подписка</a></li>
+					<li><a href="#">Реклама</a></li>
+				</ul>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+				<ul class="socials-footer">
+					<li><a href="#"><i class="sprite sprite-icon_facebook_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_twitter_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_vkontakte_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_odnoklassniki_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_youtube_footer"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Krim</title>
+	<link rel="stylesheet" href="bootstrap/less/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="fonts/Circe-Bold.css">
+	<link rel="stylesheet" href="fonts/Circe-ExtraBold.css">
+	<link rel="stylesheet" href="fonts/Circe-ExtraLight.css">
+	<link rel="stylesheet" href="fonts/Circe-Light.css">
+	<link rel="stylesheet" href="fonts/Circe-Regular.css">
+	<link rel="stylesheet" href="fonts/Circe-Thin.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/fontsmoothie.min.js" async></script>
+</head>
+<body>
+	<header class="main">
+		<nav class="navbar navbar-yellow">
+		    <div class="container">
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		            <ul class="nav navbar-nav">
+		                <li class="active"><a href="#">Главная</a></li>
+		                <li><a href="#about">Контакты</a></li>
+		                <li class="dropdown">
+		                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+		                        Пример выпадающего списка
+		                        <span class="caret"></span>
+		                    </a>
+		                    <ul class="dropdown-menu" role="menu">
+		                        <li>
+		                            <a href="#">Пример 1</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 2</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 3</a>
+		                        </li>
+		                        <li class="divider"></li>
+		                        <li class="dropdown-header">Пример 5</li>
+		                        <li>
+		                            <a href="#">Пример 6</a>
+		                        </li>
+		                        <li>
+		                            <a href="#">Пример 7</a>
+		                        </li>
+		                    </ul>
+		                </li>
+		                <li class="pull-right">
+		                	<a href="#"><i class="sprite sprite-icon_search"></i></a>
+		                </li>
+		            </ul>
+		        </div>
+		        <!--/.nav-collapse --> 
+		    </div>
+		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
+					<a href="/" class="logo"><img src="images/logo.png" alt="Logo"></a>
+				</div>
+				<div class="col-lg-6 col-md-6 col-xs-6 col-sm-6">
+					<div class="switchers pull-right">
+						<ul class="socials">
+							<li><a href="#"><i class="sprite sprite-icon_facebook"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_twitter"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_vkontakte"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_odnoklassniki"></i></a></li>
+							<li><a href="#"><i class="sprite sprite-icon_youtube"></i></a></li>
+						</ul>
+						<ul class="languages">
+							<li><a href="#">Укр</a></li>
+							<li><a href="#" class="active">Рус</a></li>
+						</ul>	
+					</div>
+				</div>
+			</div>	
+			<ul class="categories">
+				<li class="active"><a href="">События</a></li>
+				<li><a href="">Нарушение прав человека</a></li>
+				<li><a href="">Бизнес-конфликты</a></li>
+				<li><a href="">Антифейк</a></li>
+				<li><a href="">История</a></li>
+				<li><a href="">Инфографика</a></li>
+				<li><a href="">Галерея</a></li>
+				<li><a href="">Мнение</a></li>
+			</ul>
+		</div>
+	</header>
+	<section class="main">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 gutter-right-half">
+					<div class="row">
+						<article class="featured">
+							<img src="images/placeholders/featured.jpg" alt="Thumbnail featured">
+							<header>
+								<small>5 февраля 2015</small>
+								<h1>Меджлис в изгнании</h1>
+								<h2>Кто стоит за рейдерским захватом Меджлиса. Хроника событий</h2>
+							</header>
+						</article>	
+					</div>
+					<article class="main">
+						<b>- Мустафа-ага, недавно Вам исполнилось 68 лет. А на сколько Вы себя ощущаете?</b>
+						- В смысле физически? Может быть, самочувствие не соответствует возрасту, поскольку я ежедневно минут 40-50 занимаюсь спортом. Это когда в Крыму – в Киеве такой возможности нет. Мой отец прожил 83 года и точно знал, когда умрет. Оказывается, мой дед тоже прожил 83 года и перед смертью сказал моему отцу: "Дай Бог и тебе прожить столько же!". Так вот, когда отцу исполнилось 83 года, он говорит матери: "В этом году я буду умирать". Так и случилось. Мать мне после похорон говорит: "Дай Бог и тебе прожить столько же, сколько прожил отец". А я ей: "Ну что ты, мама! Я ведьуже устал…". Это было в 1982 году незадолго до моего шестого ареста, мне тогда было около 40 лет.
+
+						<b>- Из-за этой самой усталости вы заявили, что не хотите больше возглавлять Меджлис?</b>
+						- Нет, тут несколько иные соображения. Все мы на этой земле гости, и смерть может наступить в любое время. А когда происходит резкая смена руководителя – не избежать определенных сложностей в органе, который я возглавляю уже более 20-ти лет. Моя идея заключалась в том, чтобы укрепить другого на этом месте, чтобы все безболезненно проходило.
+
+						<b>- Готовите преемника?</b>
+						- Говорить о преемнике не совсем правильно, поскольку мы ведь живем не во времена Крымского ханства. Председателя Меджлиса выбирают тайным голосованием 250 делегатов Курултая (Национального съезда). Впрочем, и ханы у нас тоже выбирались, правда, не всенародно, а на совещании знатных родов Ханства. Конечно, присматриваемся к кандидатам. У одного – одни хорошие качества, у другого – другие. Их бы объединить! Но не получается.
+
+						<b>- Эти люди: Рефат Чубаров и Ремзи Ильясов?</b>
+						- На последнем Курултае я отказался баллотироваться на пост председателя Меджлиса и, поскольку в соответствии с нашими правилами самоотводы принимаются без обсуждения, то я сразу же от своего имени на этот пост выдвинул кандидатуру Рефата Чубарова. Но развернулась очень острая дискуссия, в завершение которой Курултай принял решение, что мой самоотвод не принимается. Я согласился баллотироваться снова, правда, выторговав твердое обещание, что на следующем Курултае уже точно будет избран другой председатель. Поскольку выборы председателя Меджлиса должны проходить обязательно на альтернативной основе, то были выдвинуты и другие кандидатуры. Так вот, кроме меня на том Курултае больше всего голосов собрали Рефат Чубаров и Ремзи Ильясов. Полагаю, что оба они вполне могут возглавить Меджлис. Впрочем, есть и другие достойные кандидатуры. У каждого из них преобладают какие-то положительные качества, которых недостает другим, и если между ними не будет нездоровой конкуренции, если они будут дополнять друг друга, то ничего похожего на кризис не должно произойти. Да и сам я далеко от Меджлиса удаляться не собираюсь. В меру своих возможностей буду помогать новому руководству.
+
+						<b>- Также часто называется кандидатура председателя Фонда развития Крыма Рустема Умерова.</b>
+						- Он действительно очень толковый, но Рустем видит себя больше в бизнесе и экономике, но не а политике. К тому же, он еще очень молод.
+
+						<b>- Не боитесь, что Ваш уход лишит Меджлис влияния на международном уровне? Ведь многие контакты были выстроены именно Вами, Вас знают в Европе.</b>
+						- Они же общаются не с Мустафой Джемилевым, как с личностью, а с человеком, который возглавляет представительский орган нации. Хотя, конечно, личностный фактор тоже имеет значение. У меня действительно очень широкий круг друзей почти во всех странах Европы, США, Канаде, в арабских странах. Почти во всех западных странах есть друзья, эмигрировавшие еще в диссидентские времена, а в Турции встречают со всеми протокольными почестями. Но это дело наживное. У Чубарова, конечно, такого пока нет, ибо он значительно моложе и включился в Национальное движение в перестроечные годы. Но его имя уже известно во многих странах, в высоких политических кругах Украины, пользуется высоким уважением соотечественников. Ремзи Ильясов, может быть, меньше известен за рубежом, но в Крыму его знает почти каждый соотечественник.
+
+						<b>- В 2012 году будут избирать не только главу меджлиса, но и депутатов в Верховную Раду Украины. Вы будете баллотироваться?</b>
+						- Если была бы полностью пропорциональная система, мы могли бы смело рассчитывать на два места. А поскольку будет смешанная, то по мажоритарной системе шансов практически нет, поскольку крымские татары расселены по Крыму и ни в одном избирательном округе не составляют необходимой для избрания своего кандидата численности. Поэтому, наверное, нам будут предлагать одно гарантированное место, а другое – где-то на грани проходимости. Я бы хотел, чтобы это одно место было за новым председателем Меджлиса. Имея полномочия народного депутата, неприкосновенность, обладая возможностями встречаться в любое время с руководителями государства, ему будет легче исполнять функции председателя. Поэтому вопросы о председателе и кандидатуре в народные депутаты нужно решать одновременно.
+						<footer>
+							<span class="author">Ильдар Ахметов</span>
+						</footer>
+					</article>
+					<div class="row">
+						<ul class="counts">
+							<li>
+								<span class="count">1200</span>
+								Просмотров
+							</li>
+							<li>
+								<span class="count">18</span>
+								Комментариев
+							</li>
+						</ul>
+						<div class="socials-counts pull-right">
+							<img src="images/placeholders/socials.png" alt="Ruba">	
+						</div>
+					</div>
+					<div class="comments">
+						<span class="title">Комментарии</span>
+						<form action="">
+							<div class="row gutter-bottom">
+								<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+									<img src="images/avatar.png" alt="Avatar">
+								</div>
+								<div class="col-lg-10 col-md-2 col-sm-2 col-xs-2">
+									<textarea name="" id="" cols="30" rows="8" class="w100"></textarea>
+								</div>
+							</div>	
+							<button type="submit" class="yellow-button pull-right">Добавить комментарий</button>
+						</form>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 gutter-left-half sidebar">
+					<div class="row">
+						<article class="news">
+							<img src="images/placeholders/news_3.jpg" alt="News 3">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Зависимость Крыма от Украины [инфографика]</h4>
+							</header>
+						</article>
+					</div>
+					<div class="row gutter-top">
+						<article class="news">
+							<img src="images/placeholders/news_1.jpg" alt="News 1">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Восхождение на Чатыр-Даг</h4>
+							</header>
+						</article>
+					</div>
+					<div class="row gutter-top">
+						<article class="news">
+							<img src="images/placeholders/news_3.jpg" alt="News 3">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Зависимость Крыма от Украины [инфографика]</h4>
+							</header>
+						</article>
+					</div>
+					<div class="row gutter-top">
+						<article class="news">
+							<img src="images/placeholders/news_1.jpg" alt="News 1">
+							<header>
+								<small>5 февраля 2015</small>
+								<h4>Восхождение на Чатыр-Даг</h4>
+							</header>
+						</article>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer class="main">
+		<div class="container">
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<span class="slogan">Free crimea</span>
+				<span class="copyright">© 2015 Свободный Крым. Все права защищены</span>
+			</div>
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+				<ul class="footer-menu">
+					<li><a href="#">О проекте</a></li>
+					<li><a href="#">Контакты</a></li>
+					<li><a href="#">Подписка</a></li>
+					<li><a href="#">Реклама</a></li>
+				</ul>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+				<ul class="socials-footer">
+					<li><a href="#"><i class="sprite sprite-icon_facebook_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_twitter_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_vkontakte_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_odnoklassniki_footer"></i></a></li>
+					<li><a href="#"><i class="sprite sprite-icon_youtube_footer"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
